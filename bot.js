3356e731de9ffbe647449bd014bc8e3a2ef2f1ac
Crewmasta <41262507+Crewmasta@users.noreply.github.com>
2018-08-15 21:42:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,21 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
+
+//! KinG66S.❤#0045
+var KinG66S = {};//! KinG66S.❤#0045
+client.on('guildMemberRemove', member => {//! KinG66S.❤#0045
+KinG66S[member.id] = {roles: member.roles.array()};//! KinG66S.❤#0045
+});
+//! KinG66S.❤#0045 //! KinG66S.❤#0045 //! KinG66S.❤#0045 
+client.on('guildMemberAdd', member => {//! KinG66S.❤#0045
+if(!KinG66S[member.user.id]) return;//! KinG66S.❤#0045
+console.log(KinG66S[member.user.id].roles.length);//! KinG66S.❤#0045
+for(let i = 0; i < KinG66S[member.user.id].roles.length + 1; i++) {//! KinG66S.❤#0045
+member.addRole(KinG66S[member.user.id].roles.shift());//! KinG66S.❤#0045
+}//! KinG66S.❤#0045
+});//! KinG66S.❤#0045
+
 var prefix = "r!";
 client.on('message', async message => {
   let args = message.content.split(" ");

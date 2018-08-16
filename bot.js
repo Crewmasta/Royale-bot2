f0c63f2b23b46badcb83d6d7f60733c2afd37bd8
Crewmasta <41262507+Crewmasta@users.noreply.github.com>
2018-08-16 02:40:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,45 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "r!bc")) {
+    if(message.author.id === hero.user.id) return;
+    if(message.channel.type === 'dm') return;
+    if(message.author.bot) return;
+
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(':eight_pointed_black_star: » انت لا تملك الخصائص اللازمة , يجب توفر خاصية `التحكم بأعدادات السيرفر`');
+    if(!args) return message.channel.send(':eight_pointed_black_star: » انت لم تقم بكتابة الرسالة');
+
+    try {
+      // By: iAmHeRo¹⁵ ☤#1705
+      var i = message.guild.memberCount;
+      args = args.replace('[sender]', message.author);
+      args = args.replace('[server]', message.guild.name);
+      message.channel.send(':information_source: » جاري ارسال الرسالة .. __يرجى الانتظار__');
+      setTimeout(() => {
+        message.channel.send(`:white_check_mark: » تم ارسال البرودكاست .. تم الارسال لـ ${i} شخص`);
+      }, message.guild.members.size * 1000);
+      message.guild.members.forEach(m => {
+        var bcEmbeed = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .addField(':eight_pointed_black_star: » السيرفر', `[** __${message.guild.name}__ **]`,true)
+        .addField(':eight_pointed_black_star: » المرسل', `[** __${message.author.username}__ **]`,true)
+        .addField(':eight_pointed_black_star: » الرسالة', args.replace('[user]' , m))
+        .setFooter(`${hero.user.username} :: ${new Date().toLocaleString()}`, hero.user.avatarURL)
+        .setColor('BLACK');
+        m.send(bcEmbeed).catch(e => i--);
+      });
+      // message.channel.send(`[** __Error Detected__ **] : ${e} , ${m}`)
+    } catch(e) {
+      if(e) {
+        return message.channel.send(`[** __Error Detected__ **] : ${e}`);
+      }
+    }
+  }
+});
+
+
 client.on("message", (message) => {
     if (message.content.startsWith("r!ban")) {
       if(!message.member.hasPermission('BAN_MEMBERS')) return message.reply(':warning: ماعندك الصلاحيات');
@@ -279,7 +318,8 @@ var embed = new Discord.RichEmbed ()
 **» r!mute ✮**
 **» r!unmute ✮**
 **» r!8ball ✮**
-**» r!ban ✮** `)
+**» r!ban ✮**
+**» r!bc ✮**`)
 .setColor("#66ff66")
 .addField("__r!slots__", "للعبة السلوت")
 .addField("__r!cat__", "لأرسال صورة قطة عشوائية") 
@@ -290,6 +330,7 @@ var embed = new Discord.RichEmbed ()
 .addField("__r!clear__", "لمسح الشات")
 .addField("__r!kick__", "لطرد العضو من السيرفر")
 .addField("__r!ban__", "لتبنيد العضو من السيرفر")
+.addField("__r!bc__", "لأرسال رسالة لكل اعضاء السيرفر")
 message.channel.send({embed});
 }
 });
